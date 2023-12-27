1dda4db2fb7d5f0350d9ff991b9798b80cd4445d
yu-heejin <jkgt0521@naver.com>
2023-12-27 22:46:52 +0900

    :memo: 프로젝트 이름 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  title: `WAGGLE Team 기술 블로그`,
-  description: `WAGGLE 프로젝트의 전반적인 개발 과정을 기록합니다.`,
+  title: `Travity Team 기술 블로그`,
+  description: `Travity 프로젝트의 전반적인 개발 과정을 기록합니다.`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://waggle-team.github.io/`,
+  siteUrl: `https://travity-team.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: `WAGGLE-TEAM/WAGGLE-TEAM.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `TRAVITY-TEAM/TRAVITY-TEAM.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `WAGGLE`,
+    name: `TRAVITY`,
     bio: {
       role: `Team`,
-      description: ['당신의 여행 친구'],
+      description: ['여행에 끌리다,'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
@@ -63,13 +63,13 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: 'WAGGLE Backend',
-        description: 'WAGGLE 서버 프로젝트 Repository 입니다.',
+        title: 'travity backend',
+        description: 'TRAVITY 서버 프로젝트 Repository 입니다.',
         techStack: ['Spring Boot', 'Java', 'Docker', 'GitHub Actions'],
         thumbnailUrl: '',
         links: {
           post: '',
-          github: 'https://github.com/WAGGLE-TEAM/WAGGLE_BACKEND',
+          github: 'https://github.com/TRAVITY-TEAM/travity-backend',
           googlePlay: '',
           appStore: '',
           demo: '',
